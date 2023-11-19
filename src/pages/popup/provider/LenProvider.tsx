9a4661f58df9248cf0b0d67df6b4b3eae45165d3
peachey.chen <peachey.chen@bytedance.com>
2023-11-19 04:45:59 +0300

    feat: fix getComment

--- src/pages/popup/provider/LenProvider.tsx
+++ src/pages/popup/provider/LenProvider.tsx
@@ -1,11 +1,11 @@
 "use client";
-import { LensConfig, production } from "@lens-protocol/react-web";
+import { LensConfig, development } from "@lens-protocol/react-web";
 import { bindings as wagmiBindings } from "@lens-protocol/wagmi";
 import { LensProvider as Provider } from "@lens-protocol/react-web";
 
 const lensConfig: LensConfig = {
     bindings: wagmiBindings(),
-    environment: production,
+    environment: development,
 };
 
 export function LensProvider({ children }) {
